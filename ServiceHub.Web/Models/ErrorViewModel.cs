9a1f4e9494a2fe9f4d300262d591ee74542a934b
Davi <davicairo.dev@gmil.com>
2026-02-09 14:39:45 -0300

    Initial commit

---
+++ ServiceHub.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ServiceHub.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
